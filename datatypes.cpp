f55783b2299f596f86ccf686e67b7b6f3ee3ec91
kushagra-devtron <kushagra@devtron.ai>
2023-03-24 09:08:40 +0530

    learnc++ and datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    //int, long , long long , float , double
+    //string and getline
+    // char
+    // string s1;
+    // int x = 10;
+    // cin >> s1;
+    // cout << s1;
+    string str;
+    getline(cin, str);
+    cout << str;
+    return 0;
+}
---
+++ learnc++.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std; //no need to put std everywhere now
+int main(){
+    int x,y;
+    cin >> x >> y;
+    cout << "value of x: " << x << " value of y: "<<y <<endl;
+    std::cout << "hey striver!" << std::endl;  //cout is a function which exist under the std
+    return 0;
+}
